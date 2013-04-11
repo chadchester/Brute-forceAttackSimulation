8fa4476bf48992a4e5ae524151ca56435ad04337
Samuel Deng <mr.dengshuoling@Gmail.com>
2013-04-11 13:55:20 +0800

    second

--- MultiThreadAttacker.py
+++ MultiThreadAttacker.py
@@ -1,13 +1,44 @@
 #!/usr/bin/python
 
-import socket
-import threading
+import socket;
+import threading;
 
 class AttrackerThread(threading.Thread):
-	def __init__(self,threadID, name, counter):
-		threading.Thread.__init__(self)
-		self.threadID = threadID
-		self.name = name
-		self.counter = counter
-	def run(self):
+	
+	#Initiate the thread.
+	def __init__(self,threadID, passwrds, servaddr,servport):
+		threading.Thread.__init__(self);
+		self.threadID = threadID;
+		self.password = passwrds;
+		self.servAddr = servaddr;
+		self.servPort = servport;
 
+#	#Fetch the its own part passwords from the the 'passwords' string list Given by Caller.
+#	def fetchPasswords(passs):
+#		passwords = passwrds;
+#		print 'Fetch' + len(passwords) + 'password record successfully';
+#		return;
+
+	#Start to try every password in the 'passwords'.
+	def startAttack():
+		for index in range(len(passwords)):
+			print self.threadID + ':' + index +'th password.';
+			clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+			clientSocket.connect((serveripaddr, serverpors));
+			clientSocket.send('Samuel '+passwords[index]);
+			authInfo = clientSocket.recv(100);
+			if authInfo =='OK':
+				print 'Find Successful:' + passwords[index];
+				break;
+		print 'Finish but no Answer';
+		return;
+
+	#Overrided run function
+	def run(self):
+		print 'Starting:' + self.threadID;
+#		fetchPassword();
+		startAttack();
+		print 'Stop:' + self.threadID;
+
+# fetch the file<./self.threadID.txt> and parse it into N string lists.
+def 
